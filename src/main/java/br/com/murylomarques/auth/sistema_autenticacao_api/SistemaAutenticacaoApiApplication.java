f4e3d9b5aa0e6459f0e87bbde1e8c10e6feb82c4
Murylo Marques <107437200+murylomarques@users.noreply.github.com>
2025-10-03 22:35:48 -0300

    feat: Initial project structure with Spring Boot and dependencies

---
+++ src/main/java/br/com/murylomarques/auth/sistema_autenticacao_api/SistemaAutenticacaoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.murylomarques.auth.sistema_autenticacao_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAutenticacaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAutenticacaoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/murylomarques/auth/sistema_autenticacao_api/SistemaAutenticacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.murylomarques.auth.sistema_autenticacao_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAutenticacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
